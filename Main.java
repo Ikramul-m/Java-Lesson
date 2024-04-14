db429ce003aab9043a036c8a1814e2a75c798ebc
Ikramul-m <i20727172@gmail.com>
2024-04-14 09:47:16 +0530

    Lesson

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
 
 public class Main {
     public static void main(String[] arguments) {
-        System.out.println("Hello world!");
+
+        int age;
+        age = 24;
+
+        System.out.println("I am " + age + " old.");
     }
 }
-
