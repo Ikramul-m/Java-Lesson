69112d34432a7940cfd26f341431137f62a2406e
Ikramul-m <i20727172@gmail.com>
2024-04-14 10:07:38 +0530

    Lesson

--- Main.java
+++ Main.java
@@ -3,9 +3,10 @@ public class Main {
     // static int age;
 
     public static void main(String[] arguments) {
-        int number1 = 5;
-        double number2 = number1;
+        double number1 = 5;
+        int number2 = (int) number1;
 
         System.out.println(number2);
     }
 }
+
