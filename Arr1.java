2d7743d41b41504c9217f8748d6b9aeabde8bbf9
Ikramul-m <i20727172@gmail.com>
2024-04-21 21:08:28 +0530

    Lesson

---
+++ Arr1.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Arr1 {
+
+  public static void main(String[] arguments) {
+
+    char vowels[] = {'e','a','o','u','i'};
+  
+    System.out.println(Arrays.toString(vowels));
+    
+  }
+}
+
