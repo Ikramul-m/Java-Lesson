d3348392250c2a8a768dbb7ddfe81865f8709725
Ikramul-m <i20727172@gmail.com>
2024-04-21 21:09:48 +0530

    Lesson

---
+++ Arr2.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Arr2 {
+
+  public static void main(String[] arguments) {
+
+    char vowels[] = {'e','a','o','u','i'};
+    Arrays.sort(vowels); 
+    System.out.println(Arrays.toString(vowels));
+    
+  }
+}
+
