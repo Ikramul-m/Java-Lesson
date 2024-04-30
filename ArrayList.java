0cc066b3a0ef8850d04a18c709395518cd2e1b2a
Ikramul-m <i20727172@gmail.com>
2024-04-30 11:36:38 +0530

    Array List

--- ArrayList.java
+++ ArrayList.java
@@ -1,16 +1,16 @@
-
-// import java.util.Arrays;
+import java.util.Arrays;
 import java.util.ArrayList;
 
 class Main {
-    public static void main(String[] args) {
-        ArrayList<Integer> numbers = new ArrayList<Integer>();
+  public static void main(String[] args) {
+    ArrayList<Integer> numbers = new ArrayList<Integer>();
+    
+    numbers.add(1);
+    numbers.add(2);
+    numbers.add(3);
+    numbers.add(4);
 
-        numbers.add(1);
-        numbers.add(2);
-        numbers.add(3);
-        numbers.add(4);
-
-        System.out.println(numbers.toString());
-    }
+    System.out.println(numbers.get(2));
+  }
 }
+
