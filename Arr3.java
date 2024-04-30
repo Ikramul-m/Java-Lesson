582890903173963f4a1e395c84a4134c89644763
Ikramul-m <i20727172@gmail.com>
2024-04-30 09:01:05 +0530

    Java

---
+++ Arr3.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Main {
+    public static void main(String[] args) {
+
+        char vowels[] = { 'e', 'a', 'o', 'u', 'i' };
+
+        Arrays.sort(vowels);
+
+        System.out.println(Arrays.toString(vowels));
+
+    }
+}
