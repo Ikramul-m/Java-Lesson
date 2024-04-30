69f0869dfa65cc128878a9794be21c574444dabe
Ikramul-m <i20727172@gmail.com>
2024-04-30 09:45:15 +0530

    Copy of numbers

--- FillArray.java
+++ FillArray.java
@@ -3,16 +3,13 @@ import java.util.Arrays;
 class FillArray {
     public static void main(String[] args) {
 
-        char vowels[] = { 'a', 'e', 'i', 'o', 'u' };
+        int number[] = { 1, 2, 3, 4, 5, 6 };
 
-        Arrays.sort(vowels);
+        int copyofNumbers[] = Arrays.copyOf(number, number.length);
 
-        int start = 1;
-        int end = 4;
+        Arrays.fill(number, 0);
 
-        Arrays.fill(vowels, start, end, 'x');
-
-        System.out.println(Arrays.toString(vowels));
-
+        System.out.println(Arrays.toString(number));
+        System.out.println(Arrays.toString(copyofNumbers));
     }
 }
