b4e5bd13f65c575505ea8d3441966458d7e5ac9b
Ikramul-m <i20727172@gmail.com>
2024-04-30 09:54:19 +0530

    Copy of numbers

--- FillArray.java
+++ FillArray.java
@@ -1,15 +1,20 @@
 import java.util.Arrays;
 
-class FillArray {
+class Main {
     public static void main(String[] args) {
 
         int number[] = { 1, 2, 3, 4, 5, 6 };
 
-        int copyofNumbers[] = Arrays.copyOf(number, number.length);
+        int start = 2;
+        int end = 5;
 
-        Arrays.fill(number, 3);
+        int copyofNumbers[] = Arrays.copyOf(number, number.length);
+        int copyOfRnge[] = Arrays.copyOfRange(number, start, end);
+
+        Arrays.fill(number, 0);
 
         System.out.println(Arrays.toString(number));
         System.out.println(Arrays.toString(copyofNumbers));
+        System.out.println(Arrays.toString(copyOfRnge));
     }
 }
