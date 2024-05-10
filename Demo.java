9cb11d8a9b69d381c668db298751b375e91ab8bf
Ikramul-m <i20727172@gmail.com>
2024-05-10 08:55:32 +0530

    .

---
+++ Demo.java
@@ -0,0 +1,11 @@
+public class Demo {
+    public static void main(String[] args) {
+
+        int x = 6;
+        int y = 5;
+
+        boolean result = y > x;
+
+        System.out.println(result);
+    }
+}
