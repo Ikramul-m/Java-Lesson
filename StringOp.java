00c7d299fce8455e80b67f3cecbf00a59638b7c2
Ikramul-m <i20727172@gmail.com>
2024-05-12 10:31:16 +0530

    .

--- StringOp.java
+++ StringOp.java
@@ -1,11 +1,11 @@
 
 public class StringOp {
-    public static void main(String[] args) {
-  
-      String a = "Hello";
-      String name = new String("Ikramul");
-      System.out.println(a + " " + name);
-      System.out.println(name.concat(" Haque"));
-      System.out.println(name.charAt(4));
-    }
+  public static void main(String[] args) {
+
+    String a = "Hello";
+    String name = new String("Ikramul");
+    System.out.println(a + " " + name);
+    System.out.println(name.concat(" Haque"));
+    System.out.println(name.charAt(4));
   }
+}
