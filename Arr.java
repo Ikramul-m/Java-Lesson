018eea19c27c8ded78e19406185b647bd89873db
Ikramul-m <i20727172@gmail.com>
2024-05-12 09:47:32 +0530

    .

--- Arr.java
+++ Arr.java
@@ -15,5 +15,15 @@ public class Arr {
         s1.rollNo = 1;
         s1.name = "Ikram";
         s1.marks = 78;
+
+        Student s2 = new Student();
+        s2.rollNo = 2;
+        s2.name = "Ifdal";
+        s2.marks = 70;
+    
+        Student s3 = new Student();
+        s3.rollNo = 2;
+        s3.name = "Antara";
+        s3.marks = 75;
     }
 }
