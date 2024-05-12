0d3a0a0c89f15a04d6d6023db62adeb338d1b21c
Ikramul-m <i20727172@gmail.com>
2024-05-12 09:35:08 +0530

    .

---
+++ Arr.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Main {
+  public static void main(String[] args) {
+
+    int nums[] = new int[6];
+
+    nums[0] = 9;
+    nums[1] = 8;
+    nums[2] = 5;
+    nums[3] = 0;
+
+    for (int i = 0; i < 6; i++) {
+      System.out.print(nums[i] + "  ");
+    }
+  }
+}
