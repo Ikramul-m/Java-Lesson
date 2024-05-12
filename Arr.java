5c519ff62a193682dc4a1e9f88291e1fb5a5277b
Ikramul-m <i20727172@gmail.com>
2024-05-12 09:43:17 +0530

    .

--- Arr.java
+++ Arr.java
@@ -2,18 +2,18 @@ import java.util.ArrayList;
 
 class Student {
 
-  int rollNo;
-  String name;
-  int marks;
+    int rollNo;
+    String name;
+    int marks;
 
 }
 
 public class Main {
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Student s1 = new Student();
-    s1.rollNo = 1;
-    s1.name = "Ikram";
-    s1.marks = 78;
-  }
+        Student s1 = new Student();
+        s1.rollNo = 1;
+        s1.name = "Ikram";
+        s1.marks = 78;
+    }
 }
