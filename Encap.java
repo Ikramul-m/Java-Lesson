b5be62832a4c22c5b6876c292a60d65bcbef5f37
Ikramul-m <i20727172@gmail.com>
2024-05-12 15:35:49 +0530

    .

--- Encap.java
+++ Encap.java
@@ -1,25 +1,24 @@
 class Human {
 
-    private int age = 11;
-    private String name = "John";
-  
-    public int getAge() {
-      return age;
-    }
-  
-    public String getName() {
-      return name;
-    }
-  
+  private int age = 11;
+  private String name = "John";
+
+  public int getAge() {
+    return age;
   }
-  
-  public class Encap {
-    public static void main(String[] args) {
-  
-      Human obj = new Human();
-  
-      System.out.println("Name: " + obj.getName());
-      System.out.println("Age: " + obj.getAge());
-    }
+
+  public String getName() {
+    return name;
   }
-  
+
+}
+
+public class Encap {
+  public static void main(String[] args) {
+
+    Human obj = new Human();
+
+    System.out.println("Name: " + obj.getName());
+    System.out.println("Age: " + obj.getAge());
+  }
+}
