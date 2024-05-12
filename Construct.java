112d95f15debd4831c0f206bdcf2d9229a7934c1
Ikramul-m <i20727172@gmail.com>
2024-05-12 16:08:05 +0530

    .

--- Construct.java
+++ Construct.java
@@ -2,38 +2,37 @@ class Human {
 
     private int age = 11;
     private String name = "John";
-  
+
     public Human() {
-      age = 12;
-      name = "John";
+        age = 12;
+        name = "John";
     }
-  
+
     public int getAge() {
-      return age;
+        return age;
     }
-  
+
     public void setAge(int age) {
-      this.age = age;
+        this.age = age;
     }
-  
+
     public String getName() {
-      return name;
+        return name;
     }
-  
+
     public void setName(String name) {
-      this.name = name;
+        this.name = name;
     }
-  
-  }
-  
-  public class Construct {
+
+}
+
+public class Construct {
     public static void main(String[] args) {
-  
-      Human obj = new Human();
-      Human obj1 = new Human();
-  
-      System.out.println("Name: " + obj.getName());
-      System.out.println("Age: " + obj.getAge());
+
+        Human obj = new Human();
+        Human obj1 = new Human();
+
+        System.out.println("Name: " + obj.getName());
+        System.out.println("Age: " + obj.getAge());
     }
-  }
-  
+}
