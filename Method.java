85865bb1c3ea7a0d7a68e6e8eae341424ff9989e
Ikramul-m <i20727172@gmail.com>
2024-05-13 18:52:59 +0530

    .

---
+++ Method.java
@@ -0,0 +1,13 @@
+public class Method {
+
+    public static void main(String[] args) {
+      String name = "Ikramul";
+      hello(name);
+    }
+  
+    static void hello(String x) {
+      System.out.println("Hello " + x + "!");
+    }
+  
+  }
+  
