e9c495faf977183c3a81ef88e9c128a4584a282c
Ikramul-m <i20727172@gmail.com>
2024-05-13 19:03:43 +0530

    .

---
+++ AddTwo.java
@@ -0,0 +1,16 @@
+public class AddTwo {
+
+    public static void main(String[] args) {
+      int x = 20;
+      int y = 30;
+  
+      int z = add(x, y);
+      System.out.println(z);
+    }
+  
+    static int add(int a, int b) {
+      return a + b;
+    }
+  
+  }
+  
