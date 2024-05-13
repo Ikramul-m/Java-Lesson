3d04a581aa6ab6fda5013740bea34ff6d6d2ade3
Ikramul-m <i20727172@gmail.com>
2024-05-13 08:21:54 +0530

    .

--- Inhe.java
+++ Inhe.java
@@ -1,27 +1,27 @@
 class A {
-    public A() {
-      System.out.println("in A");
-    }
-  
-    public A(int n) {
-      System.out.println("in int A");
-    }
+  public A() {
+    System.out.println("in A");
   }
-  
-  class B extends A {
-  
-    public B() {
-      System.out.println("in B");
-    }
-  
-    public B(int n) {
-      super(n);
-      System.out.println("in int B");
-    }
+
+  public A(int n) {
+    System.out.println("in int A");
   }
-  
-  public class Inhe {
-    public static void main(String[] args) {
-      B obj = new B(5);
-    }
+}
+
+class B extends A {
+
+  public B() {
+    System.out.println("in B");
   }
+
+  public B(int n) {
+    super(n);
+    System.out.println("in int B");
+  }
+}
+
+public class Inhe {
+  public static void main(String[] args) {
+    B obj = new B(5);
+  }
+}
