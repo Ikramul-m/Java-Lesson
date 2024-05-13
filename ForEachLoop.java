993b352e6e77c10a7c829300793d4fceefe83d37
Ikramul-m <i20727172@gmail.com>
2024-05-13 16:38:55 +0530

    .

---
+++ ForEachLoop.java
@@ -0,0 +1,11 @@
+
+public class ForEachLoop {
+  public static void main(String[] args) {
+
+    String[] animals = { "dog", "cat", "rat" };
+
+    for (String i : animals) {
+      System.out.println(i);
+    }
+  }
+}
