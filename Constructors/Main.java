b89f85fa198752d9c272ce7395eab4780a69a270
Ikramul-m <i20727172@gmail.com>
2024-05-13 22:08:07 +0530

    .

--- Constructors/Main.java
+++ Constructors/Main.java
@@ -3,10 +3,18 @@ public class Main {
     public static void main(String[] args) {
 
         Human human = new Human("Rick", 65, 78);
+        Human human1 = new Human("Ikramul", 24, 80);
 
         System.out.println("Name: " + human.name);
         System.out.println("Age: " + human.age);
         System.out.println("Weight: " + human.weight + " kg");
+
+        System.out.println();
+        System.out.println();
+
+        System.out.println("Name: " + human1.name);
+        System.out.println("Age: " + human1.age);
+        System.out.println("Weight: " + human1.weight);
 
     }
 }
