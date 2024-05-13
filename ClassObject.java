0ecc52387430c8181cbe614cd199b60ac9197fa2
Ikramul-m <i20727172@gmail.com>
2024-05-13 21:36:45 +0530

    .

---
+++ ClassObject.java
@@ -0,0 +1,12 @@
+class Car {
+    String name = "Ikramul";
+    String title = "Haque";
+  }
+  
+  public class ClassObject {
+    public static void main(String[] args) {
+      Car car = new Car();
+  
+      System.out.println(car.name + " " + car.title);
+    }
+  }
