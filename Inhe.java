c2dc5a2c424580560aa5367156560e06d86a918e
Ikramul-m <i20727172@gmail.com>
2024-05-13 08:06:17 +0530

    .

--- Inhe.java
+++ Inhe.java
@@ -1,18 +1,18 @@
-class A{
-    public A(){
-      System.out.println("in A");
+class A {
+    public A() {
+        System.out.println("in A");
     }
-  }
-  
-  class B extends A{
-  
-    public B(){
-      System.out.println("in B");
+}
+
+class B extends A {
+
+    public B() {
+        System.out.println("in B");
     }
-  }
-  
-  public class Inhe {
+}
+
+public class Inhe {
     public static void main(String[] args) {
-      B obj = new B();
+        B obj = new B();
     }
-  }
+}
