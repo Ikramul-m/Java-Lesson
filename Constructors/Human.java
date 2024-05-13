b5478134728f7a491ef07804e17371018bc05abf
Ikramul-m <i20727172@gmail.com>
2024-05-13 22:03:15 +0530

    .

--- Constructors/Human.java
+++ Constructors/Human.java
@@ -1,13 +1,13 @@
-public class Human{
+public class Human {
 
     String name;
     int age;
     double weight;
-  
-    Human(String name,int age, double weight){
-  
-      this.name = name;
-      this.age = age;
-      this.weight = weight;
+
+    Human(String name, int age, double weight) {
+
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
     }
-  }
+}
