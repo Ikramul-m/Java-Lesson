2ca24b269539dd83310ac856e32f331c75aa3c03
Ikramul-m <i20727172@gmail.com>
2024-05-14 12:05:31 +0530

    .

---
+++ Inheritance/Vehical.java
@@ -0,0 +1,16 @@
+public class Vehical {
+
+    double speed;
+  
+    void go() {
+      System.out.println("This vehical is moving.");
+    }
+  
+    void stop() {
+      System.out.println("This vehical stopped.");
+    }
+  }
+  
+  
+  
+  
