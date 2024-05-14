2e92c7c5b9f9936face5f9f912392fb3693813b5
Ikramul-m <i20727172@gmail.com>
2024-05-14 11:21:43 +0530

    .

--- StaticKeyword/Main.java
+++ StaticKeyword/Main.java
@@ -1,5 +1,11 @@
-package StaticKeyword;
-
 public class Main {
-    
-}
+    public static void main(String[] args) {
+  
+      Friends friend1 = new Friends("Ikramul");
+      Friends friend2 = new Friends("Ifdal");
+      Friends friend3 = new Friends("Antara");
+      
+      System.out.println(Friends.numberFreinds);
+  
+    }
+  }
