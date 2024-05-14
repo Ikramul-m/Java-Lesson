98c53a63c0dc9e53d6b510f6c81a9dbfbbf2697a
Ikramul-m <i20727172@gmail.com>
2024-05-14 12:03:53 +0530

    .

---
+++ Inheritance/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+      Car car = new Car();
+      Bycycle cycle = new Bycycle();
+      car.go();
+      cycle.stop();
+  
+      System.out.println(car.speed);
+      System.out.println(cycle.speed);
+    }
+  }
