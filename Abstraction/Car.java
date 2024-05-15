c3f5df228dfcd19842ce2232a4b0233f362cfa42
Ikramul-m <i20727172@gmail.com>
2024-05-15 07:48:54 +0530

    Abstraction

---
+++ Abstraction/Car.java
@@ -0,0 +1,13 @@
+// public class Car {
+//   {
+    
+//   };
+// }
+
+public class Car extends Vehical{
+
+    @Override
+    void go() {
+      System.out.print("The driver is driving the car");
+    }
+  }
