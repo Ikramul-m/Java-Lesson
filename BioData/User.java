c86aba7383e86ee74c0c93305ffcf7c712cbda2d
Ikramul-m <i20727172@gmail.com>
2024-05-15 09:42:41 +0530

    .

--- BioData/User.java
+++ BioData/User.java
@@ -3,12 +3,12 @@ import java.time.Period;
 
 public class User {
 
-  public String name;
-  public LocalDate birthDay;
+    public String name;
+    public LocalDate birthDay;
 
-  public int age() {
-    Period age = Period.between(this.birthDay, LocalDate.now());
+    public int age() {
+        Period age = Period.between(this.birthDay, LocalDate.now());
 
-    return age.getYears();
-  }
+        return age.getYears();
+    }
 }
