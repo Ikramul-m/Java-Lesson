65c62892dfcc5f7d3be34fc43266932152f8991d
Ikramul-m <i20727172@gmail.com>
2024-05-15 09:42:53 +0530

    .

--- BioData/Main.java
+++ BioData/Main.java
@@ -2,15 +2,16 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class Main {
-  public static void main(String[] args) {
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-    User youngerUser = new User();
+    public static void main(String[] args) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        User youngerUser = new User();
 
-    youngerUser.name = "Ikramul Haque";
+        youngerUser.name = "Ikramul Haque";
 
-    youngerUser.birthDay = LocalDate.parse("26-01-2000", formatter);
+        youngerUser.birthDay = LocalDate.parse("26-01-2000", formatter);
 
-    System.out.printf("%s was born in %s. He is now %d years old.", youngerUser.name, youngerUser.birthDay.toString(),
-        youngerUser.age());
-  }
+        System.out.printf("%s was born in %s. He is now %d years old.", youngerUser.name,
+                youngerUser.birthDay.toString(),
+                youngerUser.age());
+    }
 }
