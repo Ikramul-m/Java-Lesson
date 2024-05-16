e2476189e320c30b03361f374ede0a3c436e624f
Ikramul-m <i20727172@gmail.com>
2024-05-16 19:22:45 +0530

    Polymorphism

--- Polymorphism/Main.java
+++ Polymorphism/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-      
-      Car car  = new Car();
-      Boat boat = new Boat();
-  
-      Vehicle[] race = {car,boat};
-  
-      for(Vehicle x: race){
-        x.go();
-      }
+
+        Car car = new Car();
+        Boat boat = new Boat();
+
+        Vehicle[] race = { car, boat };
+
+        for (Vehicle x : race) {
+            x.go();
+        }
     }
-  
-  }
+
+}
