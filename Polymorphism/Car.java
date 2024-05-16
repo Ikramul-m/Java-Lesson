2827695d48a228f457b8a05096778bd1141c9e34
Ikramul-m <i20727172@gmail.com>
2024-05-16 19:21:42 +0530

    Polymorphism

---
+++ Polymorphism/Car.java
@@ -0,0 +1,8 @@
+public class Car extends Vehicle{
+  
+    @Override
+    public void go(){
+      
+      System.out.println("I'm in car");
+    }
+  }
--- character.java
+++
@@ -1,7 +0,0 @@
-public class Character {
-    public static void main(String[] args) {
-        char c = 'a';
-        int d = c;
-        System.out.println(d);
-    }
-}
