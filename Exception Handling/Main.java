056cbccfd819536a8273b9919e876caa08de0910
Ikramul-m <i20727172@gmail.com>
2024-05-16 19:55:59 +0530

    Exception Handling

--- Exception Handling/Main.java
+++ Exception Handling/Main.java
@@ -2,22 +2,25 @@ import java.util.Scanner;
 import java.util.InputMismatchException;
 
 public class Main {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
 
-        try {
-            int x = sc.nextInt();
-            int y = sc.nextInt();
+    try {
+      int x = sc.nextInt();
+      int y = sc.nextInt();
 
-            System.out.println(x / y);
-        } catch (ArithmeticException e) {
-            System.out.println("You can't divide by zero. Idiot!");
-        } catch (InputMismatchException e) {
-            System.out.println("Please enter a number.");
-        } catch (Exception e) {
-            System.out.println("Something went wrong.");
-        }
-
+      System.out.println(x / y);
+    } catch (ArithmeticException e) {
+      System.out.println("You can't divide by zero. Idiot!");
+    } catch (InputMismatchException e) {
+      System.out.println("Please enter a number.");
+    } catch (Exception e) {
+      System.out.println("Something went wrong.");
     }
+    finally{
+      sc.close();
+    }
+
+  }
 }
