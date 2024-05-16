b485de06d7df1de269017a3f4354607e221fe5b0
Ikramul-m <i20727172@gmail.com>
2024-05-16 19:46:26 +0530

    Exception Handling

--- Exception Handling/Main.java
+++ Exception Handling/Main.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-    try {
-      int x = sc.nextInt();
-      int y = sc.nextInt();
+        try {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
 
-      System.out.println(x / y);
-    } catch (ArithmeticException e) {
-      System.out.println("You can't divide by zero. Idiot!");
+            System.out.println(x / y);
+        } catch (ArithmeticException e) {
+            System.out.println("You can't divide by zero. Idiot!");
+        }
+
     }
-
-  }
 }
