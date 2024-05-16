17b8104caa7dc2c548e4c8b96b31d5a1a160469f
Ikramul-m <i20727172@gmail.com>
2024-05-16 19:46:11 +0530

    Exception Handling

---
+++ Exception Handling/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    try {
+      int x = sc.nextInt();
+      int y = sc.nextInt();
+
+      System.out.println(x / y);
+    } catch (ArithmeticException e) {
+      System.out.println("You can't divide by zero. Idiot!");
+    }
+
+  }
+}
